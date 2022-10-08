0874eac8098ccd7dbb1af361d95f6e979e786208
arekkusu6 <github.mail2116@gmail.com>
2022-10-07 22:38:37 -0400

    Junit dependency added, and test annotation working

---
+++ src/test/java/com/framework/FirstTest.java
@@ -0,0 +1,11 @@
+package com.framework;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void myFirstTest() {
+        System.out.println("myFirstTest");
+    }
+}
