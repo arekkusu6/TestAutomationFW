07dd1eebe400533c1be7fd5566ea2772f0aa8946
arekkusu6 <github.mail2116@gmail.com>
2022-10-07 22:40:47 -0400

    Example test passing/failing

--- src/test/java/com/framework/FirstTest.java
+++ src/test/java/com/framework/FirstTest.java
@@ -1,11 +1,18 @@
 package com.framework;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FirstTest {
 
     @Test
     void myFirstTest() {
-        System.out.println("myFirstTest");
+        int result = sum(2, 2);
+
+        Assertions.assertEquals(4, result);
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
     }
 }
